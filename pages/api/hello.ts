c0f9e931cc4342d669a9545a6abef1efe02e87b1
mugi-uno <mugi.uno@gmail.com>
2022-11-12 16:06:18 +0900

    initial commit

---
+++ pages/api/hello.ts
@@ -0,0 +1,20 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+
+type Data = {
+  name: string;
+};
+
+export const config = { runtime: "experimental-edge" };
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  return new Response(JSON.stringify({ name: "John Doe" }), {
+    status: 200,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+}
