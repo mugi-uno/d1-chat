c966d2c1d1101969723d38e03c05584b7b535a84
mugi-uno <mugi.uno@gmail.com>
2022-11-17 22:27:06 +0900

    add replaceWorker

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,28 +1,34 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from "next";
 import { NextRequest } from "next/server";
-
-export interface ExecutionContext {
-  env: {
-    DB: D1Database;
-  };
-}
 
 export const config = {
   runtime: "experimental-edge",
 };
 
-// eslint-disable-next-line import/no-anonymous-default-export
-export default async function (_req: NextRequest, ctx: ExecutionContext) {
-  try {
-    const { results } = await ctx.env.DB.prepare("SELECT * FROM chat ").all();
+type NextRequestWithEnv = NextRequest & {
+  env: {
+    DB: D1Database;
+  };
+};
 
-    return new Response(JSON.stringify(results), {
-      status: 200,
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
+declare var CHATDB: any;
+
+export default async function handler(req: NextRequestWithEnv, ctx: any) {
+  try {
+    // const { env } = req;
+    // const { results } = await env.DB.prepare("SELECT * FROM chat ").all();
+
+    return new Response(
+      JSON.stringify({
+        hoge: CHATDB,
+        ctx: ctx,
+      }),
+      {
+        status: 200,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
   } catch (e: any) {
     return new Response(JSON.stringify({ err: e.toString(), msg: e.message }), {
       status: 200,
